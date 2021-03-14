a39799cd8d63fb62c65694bd577ddaf97b913393
Gaurav Chaurasia <gaurav.181CV155@nitk.edu.in>
2021-03-14 14:24:22 +0530

    sum check on client-side success

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 // hello.js
 const addon = require("./build/Release/sum");
 
-console.log(addon.hello());
+// console.log(addon.hello());
 // Prints: 'world'
+
+const { render } = require('ejs');
+const express = require('express');
+const app = express();
+const path = require("path");
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get('/', (req, res) => {
+    res.render('index', {content: addon.hello()});
+})
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+});
